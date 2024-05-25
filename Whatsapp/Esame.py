f5bb6abd542801ac10cf28cd540b5901a029ae07
Nicolò <nicolo.calcagno@itsrizzoli.it>
2024-05-25 15:39:06 +0200

    proposte funzionamento

--- Whatsapp/Esame.py
+++ Whatsapp/Esame.py
@@ -27,22 +27,29 @@ while True:
                 f"5 - Esci\n\n"
                 f"Scelta: ")
     
-    # 1 - scelta aggiunta proposta
+    # 1 - scelta Cerca utente
+#vorrei prendere in input ciò he vuole cercare, poi hkeys per avere i values (tutti i nomi utenti) e cercare con pandas tutti i valori simili
     if choose == "1":
         cerca = input('Chi stai cercando')
         result = r.hget('Utenti')
+        
 
-            # 2 - scelta voto proposta
+    # 2 - scelta Aggiungi amico
+#una semplice aggiunta del amico con zset(), if else per verificare l'esistenza
     elif choose == "2":
                 pass
 
-            # 3 - scelta lettura proposte
+    # 3 - scelta Do Not Disturb
+#semplicemente una variabile in python, quando i = 1 non passano i messaggi (implementiamo un elif i = 1: pass tipo)
     elif choose == "3":
                 pass
 
+    #4 - scelta apri chat
+#penso un if che controlla l'esistenza, se non la trova crea la chat, all'interno della chat la funzione async? e la possibilità di scrivere messaggi 
     elif choose == "4":
                 pass
-                    
+          
+    #5 - esci, da implementare anche nel 4 (lascerei la chat aperta fino a che schiacciano 5)
     elif choose == "5":
                 print('bye bye')
                 break
